cb4c63c18d11f31a93a46383cd04aa9944fe209d
Nikan <32056409+Nikan888@users.noreply.github.com>
2019-01-28 22:00:32 +0300

    integrating Flask RESTful endpoints and SQLAlchemy

--- backend/src/entities/test.py
+++ backend/src/entities/test.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+from marshmallow import Schema, fields
+
 from sqlalchemy import Column, String, Date
 
 from .entity import Entity, Base
@@ -14,3 +16,11 @@ class Test(Entity, Base):
         Entity.__init__(self, created_by)
         self.name = name
         self.date = date
+
+class TestSchema(Schema):
+    id = fields.Number()
+    name = fields.Str()
+    date = fields.Date()
+    created_at = fields.DateTime()
+    updated_at = fields.DateTime()
+    last_updated_by = fields.Str()
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,29 +1,45 @@
 # coding=utf-8
+from flask import Flask, jsonify, request
 
-from entities.entity import Session, engine, Base
-from entities.test import Test
+from .entities.entity import Session, engine, Base
+from .entities.test import Test, TestSchema
 from datetime import date
 
-# generate database schema
+# creating the Flask application
+app = Flask(__name__)
+
+# if needed, generate database schema
 Base.metadata.create_all(engine)
 
-# start session
-session = Session()
+@app.route('/tests')
+def get_tests():
+    # fetching from the database
+    session = Session()
+    test_objects = session.query(Test).all()
 
-# check for existing data
-tests = session.query(Test).all()
+    # transforming into JSON-serializable objects
+    schema = TestSchema(many=True)
+    tests = schema.dump(test_objects)
 
-if len(tests) == 0:
-    # create and persist dummy test
-    python_test = Test("StreetBurger", date(2018, 11, 19), "script")
-    session.add(python_test)
-    session.commit()
+    # serializing as JSON
     session.close()
+    return jsonify(tests.data)
 
-    # reload tests
-    tests = session.query(Test).all()
 
-# show existing tests
-print('### Tests:')
-for test in tests:
-    print(f'({test.id}) {test.name} - {test.date}')
+@app.route('/tests', methods=['POST'])
+def add_test():
+    # mount test object
+    posted_test = TestSchema(only=('title', 'description'))\
+        .load(request.get_json())
+
+    test = Test(**posted_test.data, created_by="HTTP post request")
+
+    # persist test
+    session = Session()
+    session.add(test)
+    session.commit()
+
+    # return created test
+    new_test = TestSchema().dump(test).data
+    session.close()
+    return jsonify(new_test), 201
