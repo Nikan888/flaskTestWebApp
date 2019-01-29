eb3a29cc0d7fe48faad44ab898c932b69e794dae
Nikan <32056409+Nikan888@users.noreply.github.com>
2019-01-29 10:05:07 +0300

    integrating Flask and Angular

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {TestsApiService} from './tests/tests-api.service';
+import {Test} from './tests/test.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'app';
+  testsListSubs: Subscription;
+  testsList: Test[];
+
+  constructor(private testsApi: TestsApiService) {
+  }
+
+  ngOnInit() {
+    this.testsListSubs = this.testsApi
+      .getTests()
+      .subscribe(res => {
+          this.testsList = res;
+        },
+        console.error
+      );
+  }
+
+  ngOnDestroy() {
+    this.testsListSubs.unsubscribe();
+  }
 }
